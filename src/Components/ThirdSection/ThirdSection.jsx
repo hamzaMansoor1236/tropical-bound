f2ce5410f369c28e4010c2a7318804c58d05f7b9
hamzaMansoor1236 <hamza.mansoor1236@gmail.com>
2024-01-26 11:20:21 +0500

    update third section

--- src/Components/ThirdSection/ThirdSection.jsx
+++ src/Components/ThirdSection/ThirdSection.jsx
@@ -1,6 +1,6 @@
 import './ThirdSection.css'
-//import beachImage from '../../assets/imageBeach.svg'
-//import leafImage from '../../assets/leaf.svg'
+import beachImage from '../../assets/imageBeach.png'
+import leafImage from '../../assets/leaf.png'
 import background from '../../assets/rectangle.png'
 
 import React from 'react'
@@ -11,6 +11,8 @@ export const ThirdComponent = (props) => {
 
             <div className='row d-flex flex-column-reverse flex-sm-row '  >
                 <div className='col-12 col-sm-6 position-relative mt-4 mt-sm-5 '  >
+                    <img className='img-fluid style-beach-image'  src={beachImage}></img>
+                    <img className='img-fluid leaf-img-adjustment'  src={leafImage}></img>
                 </div>
 
                 <div  className='col-12 col-sm-6 d-flex flex-column justify-content-center align-items-start pt-5 negative-margin' >
